c18070daef86632c7499374b92d927c68128141a
Tahir Asadli <tahir-asadov@outlook.com>
2026-03-22 20:05:07 +0400

    Update announcement bar section

---
+++ assets/global.js
@@ -0,0 +1,14 @@
+class Announcement extends HTMLElement {
+  constructor() {
+    super();
+    this.querySelector('button').addEventListener('click', this.onCloseButtonClick.bind(this));
+    this.bindEvents();
+  }
+
+  onCloseButtonClick(event) {
+    const announcementElement = event.currentTarget.closest('.announcement');
+    announcementElement.remove();
+  }
+}
+
+customElements.define('announcement-bar', Announcement);
