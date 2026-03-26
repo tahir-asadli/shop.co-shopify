df5b1eb9380fe8f87b3aeb1101f012260d4ba86e
Tahir Asadli <tahir-asadov@outlook.com>
2026-03-27 02:11:06 +0400

    Update product page

--- assets/global.js
+++ assets/global.js
@@ -12,7 +12,83 @@ class Announcement extends HTMLElement {
 
 customElements.define('announcement-bar', Announcement);
 
+class VariantPicker extends HTMLElement {
+  constructor() {
+    super();
+    console.log('variant picker');
+  }
 
+  get sectionId() {
+    return this.dataset.sectionId;
+  }
+
+  connectedCallback() {
+    console.log('connected', this.sectionId);
+
+    this.variantSelectors = this.querySelectorAll('input[type="radio"]');
+    this.variantSelectors.forEach(selector => {
+      selector.addEventListener('change', this.handleChange.bind(this));
+    });
+  }
+
+  disconnectedCallback() {
+    console.log('disconnected', this.sectionId);
+    this.variantSelectors.forEach(selector => {
+      selector.removeEventListener('change', this.handleChange.bind(this));
+    });
+  }
+
+  handleChange(event) {
+    const input = event.currentTarget;
+    const url = `${window.location.pathname}?variant=${input.value}&section_id=${this.sectionId}`;
+    fetch(url)
+      .then(res => res.text())
+      .then(html => {
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = html;
+
+        const newSection = tempDiv.querySelector(`[section-id="${this.sectionId}"]`);
+        const currentSection = document.querySelector(`[section-id="${this.sectionId}"]`);
+        console.log(newSection, currentSection);
+
+        if (newSection && currentSection) {
+          console.log('replaced');
+
+          currentSection.replaceWith(newSection);
+        }
+
+        const newURL = new URL(url, window.location.origin);
+        newURL.searchParams.delete('section_id');
+        window.history.replaceState({}, '', newURL);
+        // const variant = data.variant;
+        // const section = data.section;
+
+        // // Update the hidden input with the new variant ID
+        // const hiddenInput = document.querySelector(`#variant-id`);
+        // hiddenInput.value = variant.id;
+
+        // // Update the price and availability
+        // const priceElement = document.querySelector('#product-price');
+        // priceElement.textContent = `$${(variant.price / 100).toFixed(2)}`;
+
+        // const addToCartButton = document.querySelector('.add-to-cart');
+        // if (variant.available) {
+        //   addToCartButton.disabled = false;
+        //   addToCartButton.textContent = 'Add to Cart';
+        //   addToCartButton.setAttribute('data-product-id', variant.id);
+        // } else {
+        //   addToCartButton.disabled = true;
+        //   addToCartButton.textContent = 'Sold Out';
+        //   addToCartButton.removeAttribute('data-product-id');
+        // }
+      })
+      .catch(err => console.error('Error fetching variant data:', err));
+    console.log('change', event.currentTarget, event.target.value, url);
+  }
+
+}
+
+customElements.define('variant-picker', VariantPicker);
 
 // Add to cart
 document.addEventListener("DOMContentLoaded", function () {
@@ -47,7 +123,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   async function addToCart(variantId, quantity = 1) {
-    const response = await fetch('/cart/add.js', {
+    const response = await fetch(window.Shopify.routes.root + 'cart/add.js', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
